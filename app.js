e2b0881536b127434be20665cacaad1db4b3d03c
Oscar Rosales <orosales2@academic.rrc.ca>
2025-02-06 18:46:11 -0600

    Reverted changes to project videos.

--- app.js
+++ app.js
@@ -21,24 +21,17 @@ const videos = document.querySelectorAll('.video');
 var windowWidth = window.innerWidth;
 var min_width = 768;
 
-if(windowWidth => min_width){
-  videos.forEach(video => {
-    video.addEventListener('mouseover', () => {
-      video.style.filter = "blur(0px)"
-    });
-
-    video.addEventListener('mouseout', () => {
-      video.style.filter = "blur(2px)"
-    });
+videos.forEach(video => {
+  video.addEventListener('mouseover', () => {
+    video.style.filter = "blur(0px)"
+    video.play()
   });
-}
 
-
-// Autoplay video
-var video = document.getElementsById("video");
-video.setAttribute("playsinline", "");
-video.setAttribute("muted", "");
-video.play();
+  video.addEventListener('mouseout', () => {
+    video.style.filter = "blur(2px)"
+    video.pause()
+  });
+});
 
 
 // Copy email to clipboard function
